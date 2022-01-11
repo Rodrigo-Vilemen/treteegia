be6bbfb8e1767a102cf0efa7d10f309aedc37c85
Rodrigo Vilemen <rodrigo.vilemen@hotmail.com>
2022-01-10 23:37:02 -0300

    update

--- frontend/src/pages/login/index.js
+++ frontend/src/pages/login/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import * as S from "./styles";
-import Botao from "../../components/Button";
 
 function Login() {
   return (
@@ -21,33 +20,36 @@ function Login() {
         </S.ContainerTextLogin>
         <S.ContainerLogin>
           <S.InputContainer>
-            <label>Login do Trello</label>
+            <label>Login do Trello:</label>
             <input type="text" />
           </S.InputContainer>
+
           <S.InputContainer>
-            <label>Senha</label>
-            <input type="text" />
+            <label>Senha:</label>
+            <input type="password" />
           </S.InputContainer>
+          
           <S.ButtonLogin>Entrar</S.ButtonLogin>
+
         </S.ContainerLogin>
       </S.ContentLogin>
       <S.ContentText>
         <div style={{ justifyContent: "flex-end" }}>
           <p>
-            No Strateegia você constrói ideias e debate colaborativamente, onde
-            o design intuitivo encontra a transformação estratégica.
+            No Strateegia você <span style={{color: '#1568B0'}}>constrói</span> ideias e <span style={{color: '#5B319E'}}>debate</span> colaborativamente, onde
+            o <span style={{color: '#EF6C00'}}>design</span> intuitivo encontra a transformação <span style={{color: '#E1005E'}}>estratégica</span>.
           </p>
         </div>
         <div>
           <p>
             Discorra seus tópicos do Trello experimentando uma plataforma para
-            criar, evoluir e adaptar projetos e aprendizados para o futuro.
+            <span style={{color: '#007AFF'}}> criar</span>, <span style={{color: '#CD3CB5'}}>evoluir</span> e <span style={{color: '#5B319E'}}>adaptar</span> projetos e aprendizados para o <span style={{color: '#CD3CB5'}}>evoluir</span> e <span style={{color: '#FDB400'}}>futuro</span>.
           </p>
         </div>
 
         <div>
-          <img></img>
-          <p></p>
+          {/* <img style={}src="../../../public/images/strateegiaAmarelo.png" alt="strateegia"/> */}
+          <p>Crie Sua Jornada no strateegia</p>
         </div>
       </S.ContentText>
     </S.Container>
--- frontend/src/pages/login/styles.js
+++ frontend/src/pages/login/styles.js
@@ -1,117 +1,128 @@
 import styled from "styled-components";
 
-export const Container = styled.div` 
-    min-height: calc(100vh - (72px * 2));
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: row;
-    
-`
+export const Container = styled.div`
+  min-height: calc(100vh - (72px * 2));
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+`;
 
 export const ContentLogin = styled.div`
-    background: #052;
-    width: 50%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+  background: #052;
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
 
 export const ContentText = styled.div`
-    width: 50%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    div{
-        width: 90%;
-        min-width: 10rem;
-        display: flex;
-        justify-content: flex-start;
-    }
-
-    p{
-        width: 100%;
-        max-width: 15rem;
-    }
-`;
-
-export const ContainerTextLogin =styled.div`
-    width:100%;
-    display: flex;
-    flex-direction: column;
-    flex-wrap: wrap;
-    justify-content: center;
-    align-items: center;
-    p{
-        width: 100%;
-        max-width: 20rem;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-    }
-`;
-
-export const ContainerLogin =styled.div`
-    width:100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-`;
-
-export const InputContainer = styled.div`
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  div {
     width: 90%;
+    min-width: 10rem;
+    display: flex;
+    justify-content: flex-start;
+  }
+
+  p {
+    width: 100%;
+    max-width: 15rem;
+  }
+`;
+
+export const ContainerTextLogin = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  p {
+    width: 100%;
     max-width: 20rem;
     display: flex;
     flex-direction: column;
     justify-content: center;
-    label{
-        width: 100%;
-    }
+  }
+`;
 
-    input{
-        width: 100%;
-    }
+export const ContainerLogin = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
+export const InputContainer = styled.div`
+  width: 90%;
+  max-width: 20rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  label {
+    width: 100%;
+  }
+
+  input {
+    width: 100%;
+  }
 `;
 
 export const ButtonLogin = styled.button`
-
+  width: 168px;
+  height: 56px;
+  border-radius: 50px;
+  background-color: #ffffff;
+  border: 3px solid #007aff;
+  box-sizing: border-box;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  /* left: 310px; */
+  /* position: absolute; */
+  /* top: 600px; */
+  /* align-items: center; */
+  font-size: 32px;
+  color: #007aff;
+  cursor: pointer;
 `;
 
+export const Content = styled.div`
+  top: 302px;
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  left: 146px;
+  bottom: 50px;
 
-const Content = styled.div`
-    top: 302px;
-    position: absolute;
-    display: flex;
-    flex-direction: column;
-    left: 146px;
-    bottom: 50px;
+  img {
+    width: 154px;
+    height: 78px;
+    margin-left: 334px;
+    top: -241px;
+  }
+  p {
+    bottom: 146px;
+    position: relative;
+    font-size: 24px;
+  }
 
-    img {
-        width: 154px;
-        height: 78px;
-        margin-left: 334px;
-        top: -241px;
-    }
-    p {
-        bottom: 146px;
-        position: relative;
-        font-size: 24px;
-    }
-
-    label {
-        margin-bottom: 10px;
-    }
-    input {
-        width: 464px;
-        height: 30px;
-        border-radius: 9px;
-        box-sizing: border-box;
-        border: 3px solid #C4C4C4;
-        background: #FFFFFF;
-        outline: none;
-    }
-`
+  label {
+    margin-bottom: 10px;
+  }
+  input {
+    width: 464px;
+    height: 30px;
+    border-radius: 9px;
+    box-sizing: border-box;
+    border: 3px solid #c4c4c4;
+    background: #ffffff;
+    outline: none;
+  }
+  
+`;
